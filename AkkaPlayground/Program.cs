cee78a14c8190422d1a78f199f3f93c47281f087
Niklaus Maurer <nick.is.normal@gmail.com>
2021-10-25 17:24:38 +0200

    adds akka.net and creates an actor to call the webservice

--- AkkaPlayground/Program.cs
+++ AkkaPlayground/Program.cs
@@ -6,57 +6,93 @@ using System.Text;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using Akka.Actor;
 
 namespace AkkaPlayground
 {
+    public class EventActor : ReceiveActor
+    {
+        public EventActor()
+        {
+            Receive<EventMessage>(message =>
+            {
+                Console.WriteLine("[Thread {0}] Message received", Thread.CurrentThread.ManagedThreadId);
+                
+                if (message.MessageType != "event" || message.EventType != "changed" ||
+                    message.ResourceType != "sensors" ||
+                    message.ResourceId != "9") return;
+
+                using var httpClient = new HttpClient()
+                {
+                    BaseAddress = new Uri("http://192.168.88.203:9080/api/84594D24F2/")
+                };
+
+                var cancellationTokenSource = new CancellationTokenSource();
+
+                if (message.ButtonEvent == 1002)
+                {
+                    var task = httpClient.PutAsync("lights/15/state", new StringContent("{ \"on\": false }", Encoding.UTF8),
+                        cancellationTokenSource.Token);
+                    task.Wait();
+                }
+                else
+                {
+                    var task = httpClient.PutAsync("lights/15/state", new StringContent("{ \"on\": true }", Encoding.UTF8),
+                        cancellationTokenSource.Token);
+                    task.Wait();
+                }
+            });
+            
+            Console.WriteLine("[Thread {0}] Constructor of EventActor terminated", Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+
+    public class EventMessage
+    {
+        public string MessageType { get; set; }
+        public string EventType { get; set; }
+        public string ResourceType { get; set; }
+        public string ResourceId { get; set; }
+        public long ButtonEvent { get; set; }
+    }
+
     internal static class Program
     {
         private static async Task Main()
         {
             using var webSocket = new ClientWebSocket();
-            using var httpClient = new HttpClient()
-            {
-                BaseAddress = new Uri("http://192.168.88.203:9080/api/84594D24F2/")
-            };
-            
-            
+
             var cancellationTokenSource = new CancellationTokenSource();
-            
             await webSocket.ConnectAsync(new Uri("ws://192.168.88.203:443"), cancellationTokenSource.Token);
+
+            var system = ActorSystem.Create("playground");
+            var eventActor = system.ActorOf<EventActor>("eventActor");
 
             while (true)
             {
                 var buffer = new byte[2048];
                 var memory = new Memory<byte>(buffer);
                 var receiveResult = await webSocket.ReceiveAsync(memory, cancellationTokenSource.Token);
-                
-                var message = Encoding.UTF8.GetString(buffer.Take(receiveResult.Count).ToArray());
-                
-                var document = JsonDocument.Parse(message);
 
-                var messageType = document.RootElement.GetProperty("t").GetString();
-                var eventType = document.RootElement.GetProperty("e").GetString();
-                var resourceType = document.RootElement.GetProperty("r").GetString();
-                var resourceId = document.RootElement.GetProperty("id").GetString();
+                var httpMessage = Encoding.UTF8.GetString(buffer.Take(receiveResult.Count).ToArray());
+                var document = JsonDocument.Parse(httpMessage);
 
-                if (messageType != "event" || eventType != "changed" || resourceType != "sensors" ||
-                    resourceId != "9") continue;
-                
+                EventMessage message = new EventMessage
+                {
+                    MessageType = document.RootElement.GetProperty("t").GetString(),
+                    EventType = document.RootElement.GetProperty("e").GetString(),
+                    ResourceType = document.RootElement.GetProperty("r").GetString(),
+                    ResourceId = document.RootElement.GetProperty("id").GetString(),
+                };
+
                 if (!document.RootElement.TryGetProperty("state", out var stateDocument)) continue;
-                    
-                var buttonEvent = stateDocument.GetProperty("buttonevent").GetInt64();
-                if (buttonEvent == 1002)
-                {
-                    await httpClient.PutAsync("lights/15/state", new StringContent("{ \"on\": false }", Encoding.UTF8), cancellationTokenSource.Token);
-                }
-                else
-                {
-                    await httpClient.PutAsync("lights/15/state", new StringContent("{ \"on\": true }", Encoding.UTF8), cancellationTokenSource.Token);
-                }
-                
-                Console.WriteLine(buttonEvent);
+                if (!stateDocument.TryGetProperty("buttonevent", out var buttonEvent)) continue;
 
-                if (cancellationTokenSource.Token.IsCancellationRequested) break;
+                message.ButtonEvent = buttonEvent.GetInt64();
+
+                //system.EventStream.Publish(message);
+                Console.WriteLine("[Thread {0}] Forwarding message", Thread.CurrentThread.ManagedThreadId);
+                eventActor.Tell(message);
             }
         }
     }
