c68c553572bce7dec9bdf3c751bd773ffd42b0b6
Niklaus Maurer <nick.is.normal@gmail.com>
2021-10-25 17:45:54 +0200

    introductes another actor to parse the WebSocket message

--- AkkaPlayground/Program.cs
+++ AkkaPlayground/Program.cs
@@ -16,7 +16,7 @@ namespace AkkaPlayground
         {
             Receive<EventMessage>(message =>
             {
-                Console.WriteLine("[Thread {0}] Message received", Thread.CurrentThread.ManagedThreadId);
+                Console.WriteLine("[Thread {0}, Actor {1}] Message received", Thread.CurrentThread.ManagedThreadId, Self.Path);
                 
                 if (message.MessageType != "event" || message.EventType != "changed" ||
                     message.ResourceType != "sensors" ||
@@ -33,17 +33,54 @@ namespace AkkaPlayground
                 {
                     var task = httpClient.PutAsync("lights/15/state", new StringContent("{ \"on\": false }", Encoding.UTF8),
                         cancellationTokenSource.Token);
+                    
+                    Console.WriteLine("[Thread {0}, Actor {1}] Request sent", Thread.CurrentThread.ManagedThreadId, Self.Path);
+                    
                     task.Wait();
                 }
                 else
                 {
                     var task = httpClient.PutAsync("lights/15/state", new StringContent("{ \"on\": true }", Encoding.UTF8),
                         cancellationTokenSource.Token);
+                    
+                    Console.WriteLine("[Thread {0}, Actor {1}] Request sent", Thread.CurrentThread.ManagedThreadId, Self.Path);
+                    
                     task.Wait();
                 }
             });
             
             Console.WriteLine("[Thread {0}] Constructor of EventActor terminated", Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+
+    public class WebSocketMessageActor : ReceiveActor
+    {
+        public WebSocketMessageActor()
+        {
+            var eventActor = Context.ActorOf<EventActor>("eventActor");
+            
+            Receive<WebSocketMessage>(webSocketMessage =>
+            {
+                Console.WriteLine("[Thread {0}, Actor {1}] Message received", Thread.CurrentThread.ManagedThreadId, Self.Path);
+                
+                var document = JsonDocument.Parse(webSocketMessage.MessageText);
+
+                EventMessage message = new EventMessage
+                {
+                    MessageType = document.RootElement.GetProperty("t").GetString(),
+                    EventType = document.RootElement.GetProperty("e").GetString(),
+                    ResourceType = document.RootElement.GetProperty("r").GetString(),
+                    ResourceId = document.RootElement.GetProperty("id").GetString(),
+                };
+
+                if (!document.RootElement.TryGetProperty("state", out var stateDocument)) return;
+                if (!stateDocument.TryGetProperty("buttonevent", out var buttonEvent)) return;
+
+                message.ButtonEvent = buttonEvent.GetInt64();
+
+                eventActor.Tell(message);
+                Console.WriteLine("[Thread {0}, Actor {1}] Message sent", Thread.CurrentThread.ManagedThreadId, Self.Path);
+            });
         }
     }
 
@@ -54,6 +91,11 @@ namespace AkkaPlayground
         public string ResourceType { get; set; }
         public string ResourceId { get; set; }
         public long ButtonEvent { get; set; }
+    }
+    
+    public class WebSocketMessage   
+    {
+        public string MessageText { get; set; }
     }
 
     internal static class Program
@@ -66,7 +108,7 @@ namespace AkkaPlayground
             await webSocket.ConnectAsync(new Uri("ws://192.168.88.203:443"), cancellationTokenSource.Token);
 
             var system = ActorSystem.Create("playground");
-            var eventActor = system.ActorOf<EventActor>("eventActor");
+            var webSocketMessageActor = system.ActorOf<WebSocketMessageActor>("webSocketMessageActor");
 
             while (true)
             {
@@ -74,25 +116,12 @@ namespace AkkaPlayground
                 var memory = new Memory<byte>(buffer);
                 var receiveResult = await webSocket.ReceiveAsync(memory, cancellationTokenSource.Token);
 
-                var httpMessage = Encoding.UTF8.GetString(buffer.Take(receiveResult.Count).ToArray());
-                var document = JsonDocument.Parse(httpMessage);
-
-                EventMessage message = new EventMessage
+                var webSocketMessage = new WebSocketMessage()
                 {
-                    MessageType = document.RootElement.GetProperty("t").GetString(),
-                    EventType = document.RootElement.GetProperty("e").GetString(),
-                    ResourceType = document.RootElement.GetProperty("r").GetString(),
-                    ResourceId = document.RootElement.GetProperty("id").GetString(),
+                    MessageText = Encoding.UTF8.GetString(buffer.Take(receiveResult.Count).ToArray())
                 };
-
-                if (!document.RootElement.TryGetProperty("state", out var stateDocument)) continue;
-                if (!stateDocument.TryGetProperty("buttonevent", out var buttonEvent)) continue;
-
-                message.ButtonEvent = buttonEvent.GetInt64();
-
-                //system.EventStream.Publish(message);
-                Console.WriteLine("[Thread {0}] Forwarding message", Thread.CurrentThread.ManagedThreadId);
-                eventActor.Tell(message);
+                
+                webSocketMessageActor.Tell(webSocketMessage);
             }
         }
     }
