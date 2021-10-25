428bd6826dcc19e96e9505561b4301c508d290d4
Niklaus Maurer <nick.is.normal@gmail.com>
2021-10-25 18:41:26 +0200

    Moves ButtonEventMessage to own File

--- AkkaPlayground/Program.cs
+++ AkkaPlayground/Program.cs
@@ -8,15 +8,6 @@ using Akka.Actor;
 
 namespace AkkaPlayground
 {
-    public class ButtonEventMessage
-    {
-        public string MessageType { get; set; }
-        public string EventType { get; set; }
-        public string ResourceType { get; set; }
-        public string ResourceId { get; set; }
-        public long ButtonEvent { get; set; }
-    }
-    
     public class WebSocketMessage   
     {
         public string MessageText { get; set; }
