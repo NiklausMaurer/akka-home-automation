0eb89509dc107ab30eb0f3461dcdfd40b20c8746
Niklaus Maurer <nick.is.normal@gmail.com>
2021-10-25 19:20:03 +0200

    code cosmetics

--- EventProcessingService/Program.cs
+++ EventProcessingService/Program.cs
@@ -7,14 +7,14 @@ using Microsoft.Extensions.Hosting;
 
 namespace EventProcessingService
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureServices((hostContext, services) => { services.AddHostedService<Worker>(); });
     }
