9ba086ea1d57c254cf04a8d207cc9c23618c3a31
Niklaus Maurer <nick.is.normal@gmail.com>
2021-10-25 18:39:52 +0200

    moves LightsActor to own class

--- AkkaPlayground/Program.cs
+++ AkkaPlayground/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Linq;
-using System.Net.Http;
 using System.Net.WebSockets;
 using System.Text;
 using System.Text.Json;
@@ -10,40 +9,6 @@ using Akka.Actor;
 
 namespace AkkaPlayground
 {
-    public class LightsActor : ReceiveActor
-    {
-        public LightsActor()
-        {
-            Receive<LightsCommandMessage>(message =>
-            {
-                Console.WriteLine("[Thread {0}, Actor {1}] Message received", Thread.CurrentThread.ManagedThreadId, Self.Path);
-
-                using var httpClient = new HttpClient()
-                {
-                    BaseAddress = new Uri("http://192.168.88.203:9080/api/84594D24F2/")
-                };
-
-                var cancellationTokenSource = new CancellationTokenSource();
-
-                var requestUri = $"lights/{message.LightId}/state";
-
-                var task = message.Command switch
-                {
-                    LightsCommandMessage.LightsCommand.TurnOn => httpClient.PutAsync(requestUri,
-                        new StringContent("{ \"on\": true }", Encoding.UTF8), cancellationTokenSource.Token),
-                    LightsCommandMessage.LightsCommand.TurnOff => httpClient.PutAsync(requestUri,
-                        new StringContent("{ \"on\": false }", Encoding.UTF8), cancellationTokenSource.Token),
-                    _ => throw new ArgumentOutOfRangeException()
-                };
-
-                Console.WriteLine("[Thread {0}, Actor {1}] Request sent", Thread.CurrentThread.ManagedThreadId, Self.Path);
-                    
-                task?.Wait();
-                if(task != null) Console.WriteLine(task.Result.StatusCode);
-            });
-        }
-    }
-
     public class LightsCommandMessage
     {
         public static LightsCommandMessage TurnOn(string lightId) => new LightsCommandMessage
