759e7bd0a719faabf90d6e158f0195f2e6eaf934
Niklaus Maurer <nick.is.normal@gmail.com>
2021-10-25 18:40:20 +0200

    moves LightsCommandMessage to own file

---
+++ AkkaPlayground/LightsCommandMessage.cs
@@ -0,0 +1,26 @@
+namespace AkkaPlayground
+{
+    public class LightsCommandMessage
+    {
+        public static LightsCommandMessage TurnOn(string lightId) => new LightsCommandMessage
+        {
+            Command = LightsCommand.TurnOn,
+            LightId = lightId
+        };
+        
+        public static LightsCommandMessage TurnOff(string lightId) => new LightsCommandMessage
+        {
+            Command = LightsCommand.TurnOff,
+            LightId = lightId
+        };
+        
+        public enum LightsCommand
+        {
+            TurnOn,
+            TurnOff
+        }
+
+        public LightsCommand Command { get; set; }
+        public string LightId { get; set; }
+    }
+}
--- AkkaPlayground/Program.cs
+++ AkkaPlayground/Program.cs
@@ -9,30 +9,6 @@ using Akka.Actor;
 
 namespace AkkaPlayground
 {
-    public class LightsCommandMessage
-    {
-        public static LightsCommandMessage TurnOn(string lightId) => new LightsCommandMessage
-        {
-            Command = LightsCommand.TurnOn,
-            LightId = lightId
-        };
-        
-        public static LightsCommandMessage TurnOff(string lightId) => new LightsCommandMessage
-        {
-            Command = LightsCommand.TurnOff,
-            LightId = lightId
-        };
-        
-        public enum LightsCommand
-        {
-            TurnOn,
-            TurnOff
-        }
-
-        public LightsCommand Command { get; set; }
-        public string LightId { get; set; }
-    }
-
     public class EventActor : ReceiveActor
     {
         public EventActor()
