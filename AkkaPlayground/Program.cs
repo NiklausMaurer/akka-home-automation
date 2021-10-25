3cecb8ced0a685053487d831601c8818d8e6c2a2
Niklaus Maurer <nick.is.normal@gmail.com>
2021-10-25 18:41:08 +0200

    Moves WebSocketMessageActor to own file

--- AkkaPlayground/Program.cs
+++ AkkaPlayground/Program.cs
@@ -2,42 +2,12 @@
 using System.Linq;
 using System.Net.WebSockets;
 using System.Text;
-using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using Akka.Actor;
 
 namespace AkkaPlayground
 {
-    public class WebSocketMessageActor : ReceiveActor
-    {
-        public WebSocketMessageActor()
-        {
-            Receive<WebSocketMessage>(webSocketMessage =>
-            {
-                Console.WriteLine("[Thread {0}, Actor {1}] Message received", Thread.CurrentThread.ManagedThreadId, Self.Path);
-                
-                var document = JsonDocument.Parse(webSocketMessage.MessageText);
-
-                ButtonEventMessage message = new ButtonEventMessage
-                {
-                    MessageType = document.RootElement.GetProperty("t").GetString(),
-                    EventType = document.RootElement.GetProperty("e").GetString(),
-                    ResourceType = document.RootElement.GetProperty("r").GetString(),
-                    ResourceId = document.RootElement.GetProperty("id").GetString(),
-                };
-
-                if (!document.RootElement.TryGetProperty("state", out var stateDocument)) return;
-                if (!stateDocument.TryGetProperty("buttonevent", out var buttonEvent)) return;
-
-                message.ButtonEvent = buttonEvent.GetInt64();
-
-                Context.System.EventStream.Publish(message);
-                Console.WriteLine("[Thread {0}, Actor {1}] Message sent", Thread.CurrentThread.ManagedThreadId, Self.Path);
-            });
-        }
-    }
-
     public class ButtonEventMessage
     {
         public string MessageType { get; set; }
