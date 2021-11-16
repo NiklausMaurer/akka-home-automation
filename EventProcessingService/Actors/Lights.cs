a76120d08ee9b62a645af3882a05e17283c6e20a
Niklaus Maurer <nick.is.normal@gmail.com>
2021-11-16 20:41:37 +0100

    moves light data fetching to Lights actor

--- EventProcessingService/Actors/Lights.cs
+++ EventProcessingService/Actors/Lights.cs
@@ -1,13 +1,29 @@
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
 using Akka.Actor;
 using EventProcessingService.Messages.Commands;
+using Newtonsoft.Json.Linq;
 
 namespace EventProcessingService.Actors
 {
     public class Lights : ReceiveActor
     {
-        public Lights(ICollection<LightDto> lights)
+        private static HttpClient _httpClient;
+
+        public Lights(CancellationToken cancellationToken)
         {
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("http://192.168.88.203:9080/api/84594D24F2/")
+            };
+            
+            var lights = FetchLightDtos(cancellationToken);
+            
             foreach (var light in lights)
             {
                 if (light.Type.Equals("On/Off plug-in unit")) continue;
@@ -22,9 +38,9 @@ namespace EventProcessingService.Actors
 
         private Dictionary<string, IActorRef> LightRefs { get; } = new();
 
-        public static Props Props(ICollection<LightDto> lights)
+        public static Props Props(CancellationToken cancellationToken)
         {
-            return Akka.Actor.Props.Create(() => new Lights(lights));
+            return Akka.Actor.Props.Create(() => new Lights(cancellationToken));
         }
 
         private void TurnLightsOn(TurnLightsOn turnOn)
@@ -41,6 +57,27 @@ namespace EventProcessingService.Actors
             {
                 light.Value.Tell(new TurnOff());
             }
+        }
+        
+        private Collection<LightDto> FetchLightDtos(CancellationToken stoppingToken)
+        {
+            var getAsyncTask = _httpClient.GetAsync("lights", stoppingToken);
+            var response = getAsyncTask .GetAwaiter().GetResult();
+            if (response.StatusCode != HttpStatusCode.OK) throw new Exception("Getting lights info failed");
+
+            var readAsyncTask = response.Content.ReadAsStringAsync(stoppingToken);
+            var content = readAsyncTask.GetAwaiter().GetResult();
+            
+            var lightsDocument = JObject.Parse(content);
+            var lightDict = lightsDocument.ToObject<Dictionary<string, LightDto>>();
+            if (lightDict is null) throw new Exception("Lights could not be parsed.");
+
+            foreach (var keyValuePair in lightDict) keyValuePair.Value.Id = keyValuePair.Key;
+
+            var lightDtos = new Collection<LightDto>(lightDict.Values.ToList());
+
+            if (lightDict is null) throw new Exception("Parsing of lghts failed");
+            return lightDtos;
         }
     }
 }
--- EventProcessingService/Worker.cs
+++ EventProcessingService/Worker.cs
@@ -1,9 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Net.WebSockets;
 using System.Text;
 using System.Threading;
@@ -13,7 +9,6 @@ using EventProcessingService.Actors;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace EventProcessingService
 {
@@ -28,32 +23,23 @@ namespace EventProcessingService
 
     public class Worker : BackgroundService
     {
-        private readonly HttpClient _httpClient;
-
         public Worker(ILogger<Worker> logger)
         {
             Logger = logger;
-
-            _httpClient = new HttpClient
-            {
-                BaseAddress = new Uri("http://192.168.88.203:9080/api/84594D24F2/")
-            };
         }
 
         private ILogger<Worker> Logger { get; }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            var lightDtos = await FetchLightDtos(stoppingToken);
-
             Logger.Log(LogLevel.Trace, "Initializing Actor System");
             var system = ActorSystem.Create("playground");
 
             var eventDispatcher = system.ActorOf<EventDispatcher>("eventDispatcher");
             system.ActorOf(TurnAllLightsOffAutomation.Props());
             system.ActorOf(TurnAllLightsOnAutomation.Props());
-            system.ActorOf(Lights.Props(lightDtos), "lights");
-            
+            system.ActorOf(Lights.Props(stoppingToken), "lights");
+
             Logger.Log(LogLevel.Trace, "Connecting to WebSocket");
             using var webSocket = new ClientWebSocket();
             var cancellationTokenSource = new CancellationTokenSource();
@@ -68,24 +54,6 @@ namespace EventProcessingService
 
                 eventDispatcher.Tell(Encoding.UTF8.GetString(buffer.Take(receiveResult.Count).ToArray()));
             }
-        }
-
-        private async Task<Collection<LightDto>> FetchLightDtos(CancellationToken stoppingToken)
-        {
-            var response = await _httpClient.GetAsync("lights", stoppingToken);
-            if (response.StatusCode != HttpStatusCode.OK) throw new Exception("Getting lights info failed");
-
-            var content = await response.Content.ReadAsStringAsync(stoppingToken);
-            var lightsDocument = JObject.Parse(content);
-            var lightDict = lightsDocument.ToObject<Dictionary<string, LightDto>>();
-            if (lightDict is null) throw new Exception("Lights could not be parsed.");
-
-            foreach (var keyValuePair in lightDict) keyValuePair.Value.Id = keyValuePair.Key;
-
-            var lightDtos = new Collection<LightDto>(lightDict.Values.ToList());
-
-            if (lightDict is null) throw new Exception("Parsing of lghts failed");
-            return lightDtos;
         }
     }
 }
